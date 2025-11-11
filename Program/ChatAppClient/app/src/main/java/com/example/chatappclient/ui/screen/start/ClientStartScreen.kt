fce1d4e8172315340f86304215ca2cedf840ee83
alta-YukaTakeuchi <yuka.takeuchi@alt-a.co.jp>
2025-11-11 17:27:46 +0900

    【ChatAppClient】起動時画面 UI作成

--- Program/ChatAppClient/app/src/main/java/com/example/chatappclient/ui/screen/start/ClientStartScreen.kt
+++ Program/ChatAppClient/app/src/main/java/com/example/chatappclient/ui/screen/start/ClientStartScreen.kt
@@ -1,2 +1,69 @@
 package com.example.chatappclient.ui.screen.start
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/**
+ * アプリ起動時画面
+ */
+@Composable
+fun ClientStartScreen() {
+    Scaffold(
+        containerColor = MaterialTheme.colorScheme.background
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier.Companion
+                .fillMaxSize()
+                .padding(paddingValues),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Companion.CenterHorizontally,
+        ) {
+            Text(
+                text = "チャットアプリ",
+                fontSize = 36.sp,
+                modifier = Modifier.Companion.padding(8.dp),
+                color = MaterialTheme.colorScheme.primary
+            )
+            Spacer(modifier = Modifier.Companion.height(72.dp))
+            ExtendedFloatingActionButton(
+                onClick = {
+                    // ユーザー名入力画面へ遷移
+                },
+                modifier = Modifier.Companion.padding(),
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            ) {
+                Text(
+                    text = "はじめる",
+                    fontSize = 22.sp,
+                    modifier = Modifier.Companion.padding(
+                        vertical = 16.dp,
+                        horizontal = 28.dp
+                    ),
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+        }
+    }
+}
+
+@Preview(device = Devices.PIXEL_2)
+@Composable
+fun ClientStartScreenPreview() {
+    ClientStartScreen()
+}
