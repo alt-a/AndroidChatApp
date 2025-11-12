179281c90f8beb10788b1ff60270ef3fb1cac620
alta-YukaTakeuchi <yuka.takeuchi@alt-a.co.jp>
2025-11-12 10:18:32 +0900

    【ChatAppClient】ユーザー名入力画面 Previewを有効にする
    プレビュー機能はViewModel処理をサポートしていないため、
    ステートレスコンポーネントを作成

--- Program/ChatAppClient/app/src/main/java/com/example/chatappclient/ui/screen/login/ClientLoginScreen.kt
+++ Program/ChatAppClient/app/src/main/java/com/example/chatappclient/ui/screen/login/ClientLoginScreen.kt
@@ -19,6 +19,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.chatappclient.ChatViewModel
 
@@ -32,12 +34,13 @@ fun ClientLoginScreen(
     viewModel: ChatViewModel,
     onConnect: () -> Unit // "() -> Unit" は「引数なし、戻り値なしの関数」という意味
 ) {
-    // 画面内で使用する一時的な状態変数
-    var ip by remember { mutableStateOf("192.168.11.16") } // IP入力用
-    var name by remember { mutableStateOf("alta") }          // 名前入力用
-
     // ★ViewModelの接続状態を監視
     val connectionStatus by viewModel.connectionStatus.collectAsState()
+
+    // ステートレスUIコンポーネントにViewModelデータを渡すための準備
+    val uiState = ClientLoginScreenUIState(
+        connectionStatus = connectionStatus
+    )
 
     // ★接続状態(connectionStatus) が変化したら実行
     LaunchedEffect(connectionStatus) {
@@ -48,6 +51,34 @@ fun ClientLoginScreen(
         // TODO: if (connectionStatus.startsWith("Error")) { ... }
         // (ここでエラーメッセージをトーストなどで表示すると親切)
     }
+
+    // ステートレスUIコンポーネント
+    ClientLoginScreenContent(
+        uiState = uiState,
+        onConnect = viewModel::connect
+    )
+}
+
+/**
+ * ユーザー名入力画面 ViewModel保持データ参照用データクラス
+ */
+data class ClientLoginScreenUIState(
+    val connectionStatus: String = "Disconnected"
+)
+
+/**
+ * ユーザー名入力画面 ステートレスUIコンポーネント
+ * @param uiState       : ViewModelが保持するUIデータ
+ * @param onConnect     : サーバー接続関数
+ */
+@Composable
+fun ClientLoginScreenContent(
+    uiState: ClientLoginScreenUIState,
+    onConnect: (ip: String, name: String) -> Unit
+) {
+    // 画面内で使用する一時的な状態変数
+    var ip by remember { mutableStateOf("192.168.11.16") } // IP入力用
+    var name by remember { mutableStateOf("alta") }          // 名前入力用
 
     Column(
         modifier = Modifier.Companion
@@ -80,13 +111,25 @@ fun ClientLoginScreen(
             onClick = {
                 // ボタンが押されたら...
                 // 1. ViewModelのconnectメソッドを呼ぶ（だけにする）
-                viewModel.connect(ip, name)
+//                viewModel.connect(ip, name)
+                onConnect(ip, name)
             },
             // ★接続中はボタンを押せなくする
-            enabled = (ip.isNotBlank() && name.isNotBlank() && connectionStatus != "Connecting...")
+            enabled = (ip.isNotBlank() && name.isNotBlank() && uiState.connectionStatus != "Connecting...")
         ) {
             // ★接続状態に応じてボタンの文字を変える
-            Text(text = if (connectionStatus == "Connecting...") "接続中..." else "接続")
+            Text(text = if (uiState.connectionStatus == "Connecting...") "接続中..." else "接続")
         }
     }
+}
+
+@Preview(device = Devices.PIXEL_2)
+@Composable
+fun ClientLoginScreenContentPreview() {
+    ClientLoginScreenContent(
+        uiState = ClientLoginScreenUIState(),
+        onConnect = { ip, name ->
+            // 何もしない
+        }
+    )
 }
