62c00755492ee5923569907f135c80c3ffbe0db9
alta-YukaTakeuchi <yuka.takeuchi@alt-a.co.jp>
2025-11-19 15:53:44 +0900

    【ChatAppServer】WebSocketフレーム用データクラス作成・FrameID継承
    FrameID	: フレーム内容識別子
    UserID	: ユーザーID通知フレーム
    UserName: ユーザー名通知フレーム

---
+++ Program/ChatAppServer/app/src/main/java/com/example/chatappserver/data/model/UserID.kt
@@ -0,0 +1,14 @@
+package com.example.chatappserver.data.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+/**
+ * ユーザーID通知フレーム データクラス
+ * @property id : ユーザーID
+ */
+@Serializable
+@SerialName("user_id")
+data class UserID(
+    val id: Int
+) : FrameID
