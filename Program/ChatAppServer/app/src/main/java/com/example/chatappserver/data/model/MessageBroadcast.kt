aec05fc827345c45f66043e7a00479953b0a83f8
alta-YukaTakeuchi <yuka.takeuchi@alt-a.co.jp>
2025-11-21 14:04:23 +0900

    【ChatAppServer】ブロードキャストメッセージ受信フレーム 内容変更

--- Program/ChatAppServer/app/src/main/java/com/example/chatappserver/data/model/MessageBroadcast.kt
+++ Program/ChatAppServer/app/src/main/java/com/example/chatappserver/data/model/MessageBroadcast.kt
@@ -4,11 +4,15 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 /**
- * ブロードキャストメッセージフレーム データクラス
+ * ブロードキャストメッセージ受信フレーム データクラス
+ * @property from       : メッセージ送信元 ユーザーID
+ * @property message    : メッセージ
+ * @property timestamp  : メッセージ送信時刻（Unix時間）
  */
 @Serializable
 @SerialName("message_broadcast")
 data class MessageBroadcast(
-    val user: String,
-    val message: String
+    @SerialName("id_from") val from: Int,
+    val message: String,
+    val timestamp: Long
 ) : FrameID
